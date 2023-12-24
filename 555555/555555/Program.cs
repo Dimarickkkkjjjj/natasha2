5e77bae28dec3e4b9eace6125de2b9b987770d92
dimarikkkkjjjj <dbasalyko@gmail.com>
2023-12-24 23:15:14 +0700

    создал консоль

---
+++ 555555/555555/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _555555
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write A");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write B");
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Select operation + - * /");
+            string op = Console.ReadLine();
+        }
+    }
+}
